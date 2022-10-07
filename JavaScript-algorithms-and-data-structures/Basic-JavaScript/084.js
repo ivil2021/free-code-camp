b47296d9a581ef700735b03900613eab245b33f5
ivil2021 <89508514326@mail.ru>
2022-10-07 22:46:17 +0200

    add new tasks

---
+++ JavaScript-algorithms-and-data-structures/Basic-JavaScript/084.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Rex",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Water", "Dogs"]
+    // Only change code above this line
+  };
