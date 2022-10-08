e239bc8afd01cb8023794d2b07bb48f6a00d91b0
ivil2021 <89508514326@mail.ru>
2022-10-08 11:53:56 +0200

    add new tasks

---
+++ JavaScript-algorithms-and-data-structures/Basic-JavaScript/093.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Billy Joel",
+    "title": "New Title",
+    "release_year": 1975,
+    "formats": [
+      "CD",
+      "MP3",
+      "LP"
+    ],
+    "platinum": true
+  }
+];
---
+++ JavaScript-algorithms-and-data-structures/Basic-JavaScript/094.js
@@ -0,0 +1,15 @@
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+let gloveBoxContents = undefined;
+
+gloveBoxContents = myStorage.car.inside["glove box"];
