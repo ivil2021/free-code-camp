dc241e95aeba33fcc2c2ddae5e24231b7de59d34
ivil2021 <89508514326@mail.ru>
2023-04-18 23:00:31 +0300

    started react

---
+++ 03-front-end-development-libraries/002.jsx
@@ -0,0 +1,9 @@
+const JSX = <div>
+  <h1></h1>
+  <p></p>
+  <ul>
+    <li>text1</li>
+    <li>text2</li>
+    <li>text3</li>
+  </ul>
+</div>;
---
+++ 03-front-end-development-libraries/003.jsx
@@ -0,0 +1,7 @@
+const JSX = (
+  <div>
+  {/* my first comment */}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
